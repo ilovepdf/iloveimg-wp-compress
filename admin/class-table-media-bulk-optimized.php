0d08b33205d5622adfccfe916499a630de0deb0d
Carlos <cmartinespinosa@gmail.com>
2019-06-20 12:45:23 +0200

    Now Bulk Optimize shows all images

--- admin/class-table-media-bulk-optimized.php
+++ admin/class-table-media-bulk-optimized.php
@@ -150,7 +150,13 @@ class iLoveIMG_Compress_Media_List_Table extends WP_List_Table {
             }
         }
 
-        $data = $wpdb->get_results( "SELECT {$wpdb->prefix}posts.* FROM {$wpdb->prefix}posts,  {$wpdb->prefix}postmeta WHERE {$wpdb->prefix}posts.post_type = 'attachment' AND {$wpdb->prefix}posts.post_mime_type IN ('image/jpg', 'image/jpeg', 'image/png', 'image/gif') AND {$wpdb->prefix}posts.ID = {$wpdb->prefix}postmeta.post_id AND {$wpdb->prefix}postmeta.meta_key = 'iloveimg_status_compress' AND {$wpdb->prefix}postmeta.meta_value < 2 " . $order, ARRAY_A );
+        $data = $wpdb->get_results( "
+            SELECT {$wpdb->prefix}posts.* 
+            FROM {$wpdb->prefix}posts
+            WHERE {$wpdb->prefix}posts.post_type = 'attachment' AND 
+                {$wpdb->prefix}posts.post_mime_type IN ('image/jpg', 'image/jpeg', 'image/png', 'image/gif') AND 
+                {$wpdb->prefix}posts.ID NOT IN (SELECT post_id FROM {$wpdb->prefix}postmeta WHERE {$wpdb->prefix}postmeta.meta_key = 'iloveimg_status_compress' AND {$wpdb->prefix}postmeta.meta_value = 2)
+                " . $order, ARRAY_A );
 
         
                 
