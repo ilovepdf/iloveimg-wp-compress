fa454d0bcfd673273893d618a8bd29670febcb35
Diego Orellano <die.ore@Mac-mini.local>
2023-10-19 10:52:25 -0300

    add prefix to variable

--- admin/views/media-bulk.php
+++ admin/views/media-bulk.php
@@ -6,16 +6,16 @@
  */
 
 // Create an instance of our package class...
-$test_list_table = new Ilove_Img_Compress_Media_List_Table();
+$ilove_img_test_list_table = new Ilove_Img_Compress_Media_List_Table();
 // Fetch, prepare, sort, and filter our data...
-$test_list_table->prepare_items();
+$ilove_img_test_list_table->prepare_items();
 
 ?>
 <div class="wrap iloveimg_settings">
 	<img src="<?php echo ILOVE_IMG_COMPRESS_PLUGIN_URL; ?>assets/images/logo.svg" class="logo" />
 	<div class="iloveimg_settings__overview">
         <?php require_once 'overview.php'; ?>
-        <?php if ( $test_list_table->total_items ) : ?>
+        <?php if ( $ilove_img_test_list_table->total_items ) : ?>
             <div class="iloveimg_settings__overview__compressAll">
                 <button type="button" id="iloveimg_allcompress" class="iloveimg-compress-all button button-small button-primary">
                     <span>Compress all</span>
@@ -27,7 +27,7 @@ $test_list_table->prepare_items();
 
     <div class="wrap">
         <form id="images-filter" method="get">
-            <?php $test_list_table->display(); ?>
+            <?php $ilove_img_test_list_table->display(); ?>
         </form>
     </div>
 </div>
