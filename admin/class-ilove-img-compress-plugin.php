3669da2b3741e2cf1859344f312ea94349ebf121
Diego Orellano <die.ore@Mac-mini.local>
2023-10-19 11:07:10 -0300

    ignore prefix into filters name WP core

--- admin/class-ilove-img-compress-plugin.php
+++ admin/class-ilove-img-compress-plugin.php
@@ -256,7 +256,7 @@ class Ilove_Img_Compress_Plugin {
 				'id'     => $attachment_id,
 			),
             'cookies'   => isset( $_COOKIE ) && is_array( $_COOKIE ) ? $_COOKIE : array(),
-            'sslverify' => apply_filters( 'https_local_ssl_verify', false ),
+            'sslverify' => apply_filters( 'https_local_ssl_verify', false ),// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
         );
         wp_remote_post( admin_url( 'admin-ajax.php' ), $args );
     }
