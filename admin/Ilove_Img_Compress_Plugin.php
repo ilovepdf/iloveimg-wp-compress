b5c5ca3d74e6221aaf8ec131ce96d4e33fd5c5d8
Diego Orellano <die.ore@Mac-mini.local>
2023-11-14 11:28:32 -0300

    update plugin to v2.0.1

--- admin/Ilove_Img_Compress_Plugin.php
+++ admin/Ilove_Img_Compress_Plugin.php
@@ -19,7 +19,7 @@ class Ilove_Img_Compress_Plugin {
 	 * @access   public
 	 * @var      string    VERSION    The current version of the plugin.
 	 */
-    const VERSION = '2.0.0';
+    const VERSION = '2.0.1';
 
     /**
 	 * The unique identifier of this plugin.
